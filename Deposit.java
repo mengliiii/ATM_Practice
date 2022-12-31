a8e09f2d31cea89e4ecc6ef702d248d33b04f1b2
Mina <114306570+mengliiii@users.noreply.github.com>
2022-12-31 12:19:28 -0500

    Create Deposit.java

---
+++ Deposit.java
@@ -0,0 +1,8 @@
+package core;
+
+public class Deposit {
+
+  //for depositing
+  //created from github ---simulate your team mates add some files adn commit and push 
+  //and pull reuqest to merge to delvelop branch
+}
